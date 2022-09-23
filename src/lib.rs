71ed23cc980d0f815bc0ba352f034fa45b8dd1a9
sander <sander.wittwer@dengineering.no>
2022-09-23 10:04:17 +0200

    replace ctypes with core::ffi which is stabilized in rust 1.64

--- src/lib.rs
+++ src/lib.rs
@@ -2,29 +2,7 @@
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
-
-pub mod ctypes {
-	pub type c_char = u8;
-	pub type c_schar = i8;
-	pub type c_uchar = u8;
-	pub type c_int = i32;
-	pub type c_uint = u32;
-	pub type c_long = i32;
-	pub type c_ulong = u32;
-	pub type c_longlong = i64;
-	pub type c_ulonglong = u64;
-	pub type c_ushort = u16;
-	pub type c_short = i16;
-
-	#[repr(u8)]
-	pub enum c_void {
-		#[doc(hidden)]
-		null = 0,
-	}
-}
-
-/// Equivalent to a C NULL pointer
-pub const NULL: ctypes::c_void = ctypes::c_void::null;
+pub use core::ffi as ctypes;
 
 /// Return the error type represented by the return value of [nrf_modem_at_printf] and [nrf_modem_at_cmd].
 ///
